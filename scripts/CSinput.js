a0e11b89728aced2348e1f09331c7e04b7bc36f5
takamineA <92371394+takamineA@users.noreply.github.com>
2021-10-27 10:38:39 +0900

    Add files via upload

---
+++ scripts/CSinput.js
@@ -0,0 +1,110 @@
+function OutputImage(target, canvas_id) {
+  const canvas = document.getElementById(canvas_id);
+  // ファイル読み込みクラス
+  var reader = new FileReader();
+  reader.onload = function () {
+    $sample = document.getElementById("sample");
+
+    // Imageクラスを使ってdiv要素にサイズを設定
+    img = new Image();
+    img.src = this.result;
+    img.onload = function () {
+      $sample.style.width = canvas.width * 0.5 + "px";
+      $sample.style.height = canvas.width * 0.5 + "px";
+    };
+    // backgroundスタイルを設定
+    $sample.style.background =
+      "url(" + this.result + ") center center / contain no-repeat";
+  };
+  // 読み込んだ画像ファイルをURLに変換
+  reader.readAsDataURL(target.files[0]);
+}
+
+// 生年月日ドロップダウンリスト生成
+
+// dropdown-toggle → birth-select
+let userBirthdayYear = document.querySelector(".birthday-year");
+let userBirthdayMonth = document.querySelector(".birthday-month");
+let userBirthdayDay = document.querySelector(".birthday-day");
+
+// 西暦取得
+const date = new Date();
+const year = date.getFullYear();
+
+// 1910年から現在の西暦+30年までoption要素を生成
+createElementsForOptions(userBirthdayYear, "  ");
+createElementsForOptions(userBirthdayYear, "??");
+for (let i = 1910; i <= year + 30; i++) {
+  createElementsForOptions(userBirthdayYear, i);
+}
+// 1月から12月までのoption要素を生成
+createElementsForOptions(userBirthdayMonth, "  ");
+createElementsForOptions(userBirthdayMonth, "??");
+for (let j = 1; j <= 12; j++) {
+  createElementsForOptions(userBirthdayMonth, j);
+}
+// 1日から31日までのoption要素を生成
+createElementsForOptions(userBirthdayDay, "  ");
+createElementsForOptions(userBirthdayDay, "??");
+for (let k = 1; k <= 31; k++) {
+  createElementsForOptions(userBirthdayDay, k);
+}
+
+// 日付の数は月によって変わるので動的に変えるようにする
+userBirthdayMonth.addEventListener("change", () => {
+  let days = createDaysForOptions(userBirthdayMonth);
+
+  // 前回までで生成されたoptionを消去
+  removeDaysElementsForOptions();
+
+  createElementsForOptions(userBirthdayDay, "  ");
+  createElementsForOptions(userBirthdayDay, "??");
+  // 月に応じた月の日付option要素を生成
+  for (let l = 1; l <= days; l++) {
+    createElementsForOptions(userBirthdayDay, l);
+  }
+});
+
+/**
+ * selectボックスのoption要素を生成するメソッド
+ * @param {int} el element
+ * @param {int} val value
+ */
+function createElementsForOptions(el, val) {
+  let op = document.createElement("option");
+  op.value = val;
+  op.text = val;
+  el.appendChild(op);
+}
+
+/**
+ * 月が替わったら日のoption要素の値を変える
+ * @param {int} el userBirthdayMonth
+ * @returns 配列の要素（日数）を返す
+ */
+function createDaysForOptions(el) {
+  let index = el.selectedIndex;
+  // 連想配列key:value
+  const month = {
+    1: 31,
+    2: 29,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+  };
+  // 要素がずれるので+1をする
+  return month[index + 1];
+}
+
+// 古いoption要素を削除するメソッド
+function removeDaysElementsForOptions() {
+  let birthday = document.getElementsByClassName("birthday-day");
+  birthday[0].innerHTML = "";
+}
--- scripts/CSmain.js
+++ scripts/CSmain.js
@@ -1,3 +1,13 @@
+// onloadイベントを追加する
+function addOnload(func, IMAGE) {
+  IMAGE.addEventListener("load", func, false);
+}
+
+/**
+ * テンプレ画像SET
+ * @param {String} container_id canvas親要素divのid
+ * @param {String} canvas_id canvasのid
+ */
 function loadImage(container_id, canvas_id) {
   // 画像を読み込んでImageオブジェクトを作成する
   var image = new Image();
@@ -10,25 +20,270 @@ function loadImage(container_id, canvas_id) {
     // 親要素のサイズをcanvasに指定
     canvas.width = container.clientWidth;
     canvas.height = container.clientHeight;
+    // 背景を白色に描画
+    ctx.fillStyle = "rgb(255,255,255)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     // キャンバスに画像を描画（開始位置0,0）
     // 第４引数、第５引数の指定サイズにリサイズ
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    // // fillRect test
+    // ctx.fillStyle = "rgba(0,255,255,0.5)";
+    // let X = canvas.width * 0.039 + canvas.width * 0.0818 * 5;
+    // let Y = canvas.width * 0.65 - canvas.width * 0.0183 * 17;
+    // let W = canvas.width * 0.072;
+    // let H = canvas.width * 0.0183 * 18;
+    // ctx.fillRect(X, Y, W, H);
   };
 }
 
 // キャンバスに文字を描く
-function drawText(canvas_id, text_id) {
-  var canvas = document.getElementById(canvas_id);
+function drawText(
+  color_id,
+  image_id,
+  canvas_id,
+  LName_id,
+  FName_id,
+  Lread_id,
+  Fread_id,
+  Sex_id,
+  Year_id,
+  Month_id,
+  Day_id,
+  height_id,
+  PName_id,
+  other_id,
+  STR_id,
+  CON_id,
+  POW_id,
+  DEX_id,
+  APP_id,
+  SIZ_id,
+  INT_id,
+  EDU_id
+) {
+  // 引数取得
+  var color = document.getElementById(color_id);
+  var File = document.getElementById(image_id);
+  var LName = document.getElementById(LName_id);
+  var FName = document.getElementById(FName_id);
+  var Lread = document.getElementById(Lread_id);
+  var Fread = document.getElementById(Fread_id);
+  var Sex = document.getElementById(Sex_id);
+  var Year = document.getElementById(Year_id);
+  var Month = document.getElementById(Month_id);
+  var Day = document.getElementById(Day_id);
+  var Height = document.getElementById(height_id);
+  var PLname = document.getElementById(PName_id);
+  var OtText = document.getElementById(other_id);
+  var STR = document.getElementById(STR_id);
+  var CON = document.getElementById(CON_id);
+  var POW = document.getElementById(POW_id);
+  var DEX = document.getElementById(DEX_id);
+  var APP = document.getElementById(APP_id);
+  var SIZ = document.getElementById(SIZ_id);
+  var INT = document.getElementById(INT_id);
+  var EDU = document.getElementById(EDU_id);
+
+  // キャラシテンプレ再ロード
+  const canvas = document.getElementById(canvas_id);
   var ctx = canvas.getContext("2d");
-  var text = document.getElementById(text_id);
-  // 文字スタイルを指定
-  ctx.font = "80px serif";
-  ctx.fillStyle = "#ff4040";
-  // 文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter）
-  ctx.textBaseline = "center";
-  ctx.textAlign = "center";
-  // 座標を指定して文字を描く（座標は画像の中心に）
-  var x = canvas.width / 2;
-  var y = canvas.height / 2;
-  ctx.fillText(text.value, x, y);
+  const image = new Image();
+  image.src = "../images/CharaSheetTemplate.png";
+
+  // 塗りつぶし
+  ctx.fillStyle = "rgb(255,255,255)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = color.value;
+
+  function fillStatus(val, num) {
+    if (parseInt(val) > 18) {
+      val = "18";
+    }
+    let X = canvas.width * 0.039 + canvas.width * 0.0818 * (num - 1);
+    let Y = canvas.width * 0.65 - canvas.width * 0.0183 * (parseInt(val) - 1);
+    let W = canvas.width * 0.072;
+    let H = canvas.width * 0.0183 * parseInt(val);
+    ctx.fillRect(X, Y, W, H);
+  }
+
+  fillStatus(STR.value, 1);
+  fillStatus(CON.value, 2);
+  fillStatus(POW.value, 3);
+  fillStatus(DEX.value, 4);
+  fillStatus(APP.value, 5);
+  fillStatus(SIZ.value, 6);
+  fillStatus(INT.value, 7);
+  fillStatus(EDU.value, 8);
+
+  // PC画像描画
+
+  var PCimage = new Image();
+  PCimage.src = "../images/example.jpg";
+
+  addOnload(canvas_prev, image);
+  PCimage.onload = PC_prev();
+
+  function PC_prev() {
+    let X = canvas.width * 0.0327;
+    let Y = canvas.width * 0.023;
+    let W = canvas.width * 0.263;
+    let H = canvas.width * 0.263;
+    // PC画像のロードが完了したら描画
+    ctx.drawImage(PCimage, X, Y, W, H);
+  }
+
+  function canvas_prev() {
+    // 画像ロードが完了してから描画
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    // name
+    // 文字スタイルを指定
+    let size = canvas.width * 0.045;
+    let x = canvas.width * 0.475;
+    let y = canvas.height * 0.0945;
+    fillcenterText(ctx, LName.value + " " + FName.value, x, y, size);
+
+    // read
+    size = canvas.width * 0.015;
+    x = canvas.width * 0.475;
+    y = canvas.width * 0.055;
+    fillcenterText(ctx, Lread.value + "      " + Fread.value, x, y, size);
+
+    // sex
+    size = canvas.width * 0.045;
+    x = canvas.width * 0.683;
+    y = canvas.width * 0.0875;
+    fillcenterText(ctx, Sex.value, x, y, size);
+
+    // height
+    size = canvas.width * 0.035;
+    x = canvas.width * 0.765;
+    y = canvas.width * 0.0845;
+    fillcenterText(ctx, Height.value, x, y, size);
+
+    // birth
+    size = canvas.width * 0.025;
+    x = canvas.width * 0.885;
+    y = canvas.width * 0.07;
+    if (Year.value == "  " && Month.value == "  " && Day.value == "  ") {
+    } else {
+      fillcenterText(
+        ctx,
+        Year.value + "/     \n       " + Month.value + "/" + Day.value,
+        x,
+        y,
+        size
+      );
+    }
+
+    // other date
+    size = canvas.width * 0.02;
+    x = canvas.width * 0.325;
+    y = canvas.width * 0.133;
+    filltopleftText(ctx, OtText.value, x, y, size);
+
+    // PLname
+    size = canvas.width * 0.025;
+    x = canvas.width * 0.89;
+    y = canvas.width * 0.27;
+    fillcenterText(ctx, PLname.value, x, y, size);
+
+    // SAN
+    size = canvas.width * 0.07;
+    x = canvas.width * 0.883;
+    y = canvas.width * 0.383;
+    fillcenterText(ctx, parseInt(POW.value) * 5 + "", x, y, size);
+
+    // HP
+    size = canvas.width * 0.06;
+    x = canvas.width * 0.763;
+    y = canvas.width * 0.482;
+    let HP = (parseInt(CON.value) + parseInt(SIZ.value)) / 2;
+    HP = Math.ceil(HP);
+    fillcenterText(ctx, HP + "", x, y, size);
+
+    // MP
+    size = canvas.width * 0.06;
+    x = canvas.width * 0.898;
+    y = canvas.width * 0.482;
+    fillcenterText(ctx, POW.value, x, y, size);
+
+    // IDEA
+    size = canvas.width * 0.06;
+    x = canvas.width * 0.767;
+    y = canvas.width * 0.5857;
+    fillcenterText(ctx, parseInt(INT.value) * 5 + "", x, y, size);
+
+    // 知識
+    size = canvas.width * 0.06;
+    x = canvas.width * 0.902;
+    y = canvas.width * 0.5857;
+    fillcenterText(ctx, parseInt(EDU.value) * 5 + "", x, y, size);
+
+    // 幸運
+    size = canvas.width * 0.06;
+    x = canvas.width * 0.767;
+    y = canvas.width * 0.687;
+    fillcenterText(ctx, parseInt(POW.value) * 5 + "", x, y, size);
+
+    // DB
+    size = canvas.width * 0.04;
+    x = canvas.width * 0.9;
+    y = canvas.width * 0.68;
+    let point = parseInt(STR.value) + parseInt(SIZ.value);
+    let DB;
+    if (point <= 12) {
+      DB = "-1D6";
+    } else if (point <= 16) {
+      DB = "-1D4";
+    } else if (point <= 24) {
+      y = canvas.width * 0.675;
+      DB = "--";
+    } else if (point <= 32) {
+      DB = "+1D4";
+    } else if (point <= 40) {
+      DB = "+1D6";
+    } else {
+      DB = "";
+    }
+    fillcenterText(ctx, DB, x, y, size);
+  }
+}
+
+/**
+ * テキスト描画で改行させるためのメソッド
+ * @param {ctx} context canvas
+ * @param {String} text 書き出すテキスト
+ * @param {int} x x軸
+ * @param {int} y y軸
+ */
+function fillTextLine(context, text, x, y) {
+  // \nで分割して配列にする
+  var textList = text.split("\n");
+  // あ はフォントサイズを取得するのに利用
+  var lineHeight = context.measureText("あ").width;
+  textList.forEach(function (text, i) {
+    // 配列を順番に読み出して、y（高さ)を計算しながら描画
+    context.fillText(text, x, y + lineHeight * i);
+  });
+}
+
+// 左上基準の描画
+function filltopleftText(context, text, x, y, size) {
+  context.textBaseline = "top";
+  context.textAlign = "left";
+  context.font = size + "px 'RocknRoll One', sans-serif";
+  fillTextLine(context, text, x, y);
+}
+
+// 中央基準の描画
+function fillcenterText(context, text, x, y, size) {
+  // 文字の配置を指定
+  //（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter）
+  context.textBaseline = "alphabetic";
+  context.textAlign = "center";
+  context.font = size + "px 'RocknRoll One', sans-serif";
+  fillTextLine(context, text, x, y);
 }
